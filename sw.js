db6d727aad5c510292842075a137e1cd0b417398
maevehuang10 <maevehuang10@gmail.com>
2026-09-20 09:52:40 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='maeve-pocket-2714e8f4eb36';
-const ASSETS=['./','./index.html','./data.json','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='maeve-pocket-8495a88b9b0e';
+const ASSETS=['./','./index.html','./classroom.html','./data.json','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('maeve-pocket-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
